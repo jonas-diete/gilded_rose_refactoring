69180cce769b23976c1fe96945e606bd6454dde9
Jonas Diete <jonas.diete@gmail.com>
2022-10-25 15:53:28 +0100

    quality cannot drop below 0

--- src/shop.js
+++ src/shop.js
@@ -4,11 +4,26 @@ class Shop {
   }
 
   updateQuality() {
+    const maxQuality = 50;
+
     this.items.forEach((item) => {
-      item.quality -= 1;
+      if (item.sellIn < 0) {
+        item = this.lowerQuality(item, -2);
+      } else {
+        item = this.lowerQuality(item);
+      }
+      item.sellIn--;
     })
 
     return this.items;
+  }
+
+  lowerQuality(item, rate = -1) {
+    item.quality += rate;
+    if (item.quality < 0) {
+      item.quality = 0;
+    }
+    return item;
   }
 }
 
--- test/shop.test.js
+++ test/shop.test.js
@@ -39,4 +39,63 @@ describe('Shop', () => {
     expect(updatedItems[0].quality).toBe(4);
     expect(updatedItems[1].quality).toBe(12);
   })
+
+  it('lowers the quality for multiple items', () => {
+    let fakeItem1 = {
+      name: 'Lembas Bread',
+      sellIn: 10,
+      quality: 5
+    }
+    let fakeItem2 = {
+      name: 'Rusty Axe',
+      sellIn: 25,
+      quality: 13
+    }
+    const shop = new Shop([fakeItem1, fakeItem2]);
+    const updatedItems = shop.updateQuality();
+    expect(updatedItems[0].quality).toBe(4);
+    expect(updatedItems[1].quality).toBe(12);
+  })
+
+  it('lowers the sellIn for multiple items', () => {
+    let fakeItem1 = {
+      name: 'Lembas Bread',
+      sellIn: 10,
+      quality: 5
+    }
+    let fakeItem2 = {
+      name: 'Rusty Axe',
+      sellIn: 25,
+      quality: 13
+    }
+    const shop = new Shop([fakeItem1, fakeItem2]);
+    const updatedItems = shop.updateQuality();
+    expect(updatedItems[0].sellIn).toBe(9);
+    expect(updatedItems[1].sellIn).toBe(24);
+  })
+
+  it('lowers the quality by 2 when sellIn is negative', () => {
+    let fakeItem1 = {
+      name: 'Leather Belt',
+      sellIn: -1,
+      quality: 12
+    }
+    const shop = new Shop([fakeItem1]);
+    const updatedItems = shop.updateQuality();
+    expect(updatedItems[0].sellIn).toBe(-2);
+    expect(updatedItems[0].quality).toBe(10);
+  })
+
+  it('quality of an item doesnt go below 0', () => {
+    let fakeItem1 = {
+      name: 'Snake Elixir',
+      sellIn: 10,
+      quality: 2
+    }
+    const shop = new Shop([fakeItem1]);
+    shop.updateQuality();
+    shop.updateQuality();
+    const updatedItems = shop.updateQuality();
+    expect(updatedItems[0].quality).toBe(0);
+  })
 }) 
