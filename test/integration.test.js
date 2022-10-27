5a5d37162f8ff6c01cac8ef768c3073a6d8754d7
Jonas Diete <jonas.diete@gmail.com>
2022-10-27 18:02:57 +0100

    refactored with new child classes, inheritance, polymorphism

---
+++ test/integration.test.js
@@ -0,0 +1,146 @@
+const Shop = require('../src/shop');
+const StockItem = require('../src/stockItem');
+const BetterWithAge = require('../src/betterWithAge');
+const Sulfuras = require('../src/sulfuras');
+const BackstagePass = require('../src/backstagePass');
+
+describe('Shop', () => {
+  it('lowers the quality for an item', () => {
+    let item = new StockItem('Lembas Bread', 10, 5);
+    const gildedRose = new Shop([item]);
+    const updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].quality).toBe(4);
+  })
+
+  it('lowers the quality for an item', () => {
+    let item = new StockItem('Rusty Axe', 25, 13);
+    const gildedRose = new Shop([item]);
+    const updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].quality).toBe(12);
+  })
+
+  it('lowers the quality for multiple items', () => {
+    let item1 = new StockItem('Lembas Bread', 10, 5);
+    let item2 = new StockItem('Rusty Axe', 25, 13);
+    const gildedRose = new Shop([item1, item2]);
+    const updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].quality).toBe(4);
+    expect(updatedItems[1].quality).toBe(12);
+  })
+
+  it('lowers the quality for multiple items, multiple times', () => {
+    let item1 = new StockItem('Lembas Bread', 10, 5);
+    let item2 = new StockItem('Rusty Axe', 25, 13);
+    const gildedRose = new Shop([item1, item2]);
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    const updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].quality).toBe(2);
+    expect(updatedItems[1].quality).toBe(10);
+  })
+
+  it('lowers the sellIn for multiple items', () => {
+    let item1 = new StockItem('Lembas Bread', 10, 5);
+    let item2 = new StockItem('Rusty Axe', 25, 13);
+    const gildedRose = new Shop([item1, item2]);
+    const updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].sellIn).toBe(9);
+    expect(updatedItems[1].sellIn).toBe(24);
+  })
+
+  it('lowers the sellIn for multiple items, multiple times', () => {
+    let item1 = new StockItem('Lembas Bread', 10, 5);
+    let item2 = new StockItem('Rusty Axe', 25, 13);
+    const gildedRose = new Shop([item1, item2]);
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    const updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].sellIn).toBe(7);
+    expect(updatedItems[1].sellIn).toBe(22);
+  })
+
+  it('lowers the quality by 2 when sellIn is 0 or negative', () => {
+    let item = new StockItem('Leather Belt', -1, 12);
+    const gildedRose = new Shop([item]);
+    const updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].sellIn).toBe(-2);
+    expect(updatedItems[0].quality).toBe(10);
+  })
+
+  it('quality of an item doesnt go below 0', () => {
+    let item = new StockItem('Snake Elixir', 10, 2);
+    const gildedRose = new Shop([item]);
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    const updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].quality).toBe(0);
+  })
+
+  it('Aged Brie increases in quality', () => {
+    let item = new BetterWithAge('Aged Brie', 31, 8)
+    const gildedRose = new Shop([item]);
+    gildedRose.updateQuality();
+    const updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].quality).toBe(10);
+  })
+
+  it('Aged Brie increases in quality even with negative sellIn', () => {
+    let item = new BetterWithAge('Aged Brie', -3, 8)
+    const gildedRose = new Shop([item]);
+    gildedRose.updateQuality();
+    const updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].quality).toBe(10);
+  })
+
+  it('Quality doesnt increase beyond 50', () => {
+    let item = new BetterWithAge('Aged Brie', 12, 49)
+    const gildedRose = new Shop([item]);
+    gildedRose.updateQuality();
+    const updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].quality).toBe(50);
+  })
+
+  it('Sulfuras values dont change', () => {
+    const item = new Sulfuras('Sulfuras', 100, 32);
+    const gildedRose = new Shop([item]);
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    const updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].quality).toBe(32);
+    expect(updatedItems[0].sellIn).toBe(100);
+  })
+
+  it('Backstage Passes increase in quality if sellIn is not negative', () => {
+    let item = new BackstagePass('Backstage passes', 30, 10);
+    const gildedRose = new Shop([item]);
+    gildedRose.updateQuality();
+    gildedRose.updateQuality();
+    const updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].quality).toBe(13);
+    expect(updatedItems[0].sellIn).toBe(27);
+  })
+
+  it('Backstage Pass quality is 0 if sellIn is 0 or negative', () => {
+    let item = new BackstagePass('Backstage passes', 0, 10);
+    const gildedRose = new Shop([item]);
+    const updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].quality).toBe(0);
+    expect(updatedItems[0].sellIn).toBe(-1);
+  })
+
+  it('Backstage Passes increase in quality by 3 if sellIn is 5 or lower but above 0', () => {
+    let item = new BackstagePass('Backstage passes', 4, 10);
+    const gildedRose = new Shop([item]);
+    const updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].quality).toBe(13);
+    expect(updatedItems[0].sellIn).toBe(3);
+  })
+
+  it('Backstage Passes increase in quality by 2 if sellIn is 10 or lower but above 5', () => {
+    let item = new BackstagePass('Backstage passes', 8, 10);
+    const gildedRose = new Shop([item]);
+    const updatedItems = gildedRose.updateQuality();
+    expect(updatedItems[0].quality).toBe(12);
+    expect(updatedItems[0].sellIn).toBe(7);
+  })
+}) 
--- test/texttest_fixture.js
+++ test/texttest_fixture.js
@@ -1,17 +1,22 @@
-const { Shop, Item } = require("../src/gilded_rose");
+const Shop = require("../src/shop");
+const StockItem = require("../src/StockItem");
+const BetterWithAge = require("../src/betterWithAge");
+const Sulfuras = require("../src/sulfuras");
+const BackstagePass = require("../src/backstagePass");
+
 
 const items = [
-  new Item("+5 Dexterity Vest", 10, 20),
-  new Item("Aged Brie", 2, 0),
-  new Item("Elixir of the Mongoose", 5, 7),
-  new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-  new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-  new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+  new StockItem("+5 Dexterity Vest", 10, 20),
+  new BetterWithAge("Aged Brie", 4, 39),
+  new StockItem("Elixir of the Mongoose", 5, 7),
+  new Sulfuras("Sulfuras, Hand of Ragnaros", 0, 80),
+  new Sulfuras("Sulfuras, Hand of Ragnaros", -1, 80),
+  new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+  new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+  new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 5, 49),
 
   // This Conjured item does not work properly yet
-  new Item("Conjured Mana Cake", 3, 6),
+  new StockItem("Conjured Mana Cake", 3, 6),
 ];
 
 const days = Number(process.argv[2]) || 2;
