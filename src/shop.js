48c14ea8798e2cb8c66cc088b523251514e18c4f
Jonas Diete <jonas.diete@gmail.com>
2022-10-28 11:01:03 +0100

    refactored now classing items inside shop

--- src/shop.js
+++ src/shop.js
@@ -1,14 +1,33 @@
+const StockItem = require('../src/stockItem');
+const BetterWithAge = require('../src/betterWithAge');
+const Sulfuras = require('../src/sulfuras');
+const BackstagePass = require('../src/backstagePass');
+const ConjuredItem = require('../src/conjuredItem')
+
 class Shop {
   constructor(items = []) {
-    this.items = items;
+    this.items = this.classItems(items);
+  }
+
+  classItems(items) {
+    const classedItems = items.map((item) => {
+      if (item.name === "Aged Brie") {
+        return new BetterWithAge(item.name, item.sellIn, item.quality);
+      } else if (item.name.includes('Backstage passes')) {
+        return new BackstagePass(item.name, item.sellIn, item.quality);
+      } else if (item.name.includes('Sulfuras')) {
+        return new Sulfuras(item.name, item.sellIn, item.quality);
+      } else if (item.name.includes('Conjured')) {
+        return new ConjuredItem(item.name, item.sellIn, item.quality);
+      } else {
+        return new StockItem(item.name, item.sellIn, item.quality);
+      }
+    })
+    return classedItems;
   }
 
   updateQuality() {
-    this.items.forEach((item) => {
-      item.updateValues();
-    })
-
-    return this.items;
+    this.items.forEach(item => item.updateValues())
   }
 }
 
